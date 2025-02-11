65b0b7929232ebd93f60fda40207c697c572bf07
Kondol <kyk9702@gmail.com>
2025-02-12 03:13:45 +0900

    Steo 11 수평스크롤

--- PlanetPedia/Controller/PlanetDetailViewController.swift
+++ PlanetPedia/Controller/PlanetDetailViewController.swift
@@ -29,8 +29,6 @@ class PlanetDetailViewController: UIViewController {
     // var planet: Planet?
     
     func setupLayout() {
-        
-        
         let layout = UICollectionViewCompositionalLayout{ sectionIndex, environment in
             switch sectionIndex {
             case 1:
@@ -58,6 +56,23 @@ class PlanetDetailViewController: UIViewController {
                 section.interGroupSpacing = 20
                 
                 return section
+                
+            case 2:
+                var size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(200))
+                
+                let item = NSCollectionLayoutItem(layoutSize: size)
+                
+                size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.6), heightDimension: .estimated(200))
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+                section.interGroupSpacing = 20
+                section.orthogonalScrollingBehavior = .continuous
+                
+                return section
+                
             default :
                 let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(200))
                 
